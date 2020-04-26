f4daf2bc0d2f471df39db509cd487a82981640bb
Cezara <cezaraluca@yahoo.com>
2020-04-26 15:18:15 +0300

    ex 2

--- lab7/laborator7.js
+++ lab7/laborator7.js
@@ -1,4 +1,37 @@
-//2. Generati toate numerele divizibile cu 10 de la 1 la 1000 folosind compunerea functiilor. Comparati timpii de executie cu cei de la lab anterior
+//2. Generati toate numerele divizibile cu 10 de la 1 la 1000
+//folosind compunerea functiilor.
+//Comparati timpii de executie cu cei de la lab anterior.
+
+//ex lab anterior
+
+function x(cond, then) {
+  if (cond) then();
+}
+function div(max, action) {
+  for (var i = 1; i <= max; i++) {
+    action(i);
+  }
+}
+
+div(1000, (n) => {
+  x(n % 10 === 0, () => {
+    //arr.push(n);
+    console.log(n);
+  });
+});
+
+//compunere functii
+
+function divNumar(n) {
+  return (m) => {
+    for (let i = 1; i <= m; i++)
+      if (i % n === 0) {
+        console.log(i);
+      }
+  };
+}
+//const divNumar10 = divNumar(10);
+divNumar(10)(1000);
 
 //3.
 
