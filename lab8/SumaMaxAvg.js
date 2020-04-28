fca04ab825f79d63d309d23ee014bf069f6a97d1
Cezara <cezaraluca@yahoo.com>
2020-04-28 12:39:07 +0300

    sum max avg

---
+++ lab8/SumaMaxAvg.js
@@ -0,0 +1,50 @@
+var _ = require("underscore");
+
+//Recursiv - suma elementelor unui sir
+
+const suma = (arr) => (arr.length === 0 ? 0 : arr[0] + suma(arr.slice(1)));
+console.log(suma([1, 2, 3, 4, 5]));
+console.log(suma(_.range(10)));
+
+//Recursiv - maximul elementelor unui sir 1
+
+function max1(arr) {
+  if (arr.length === 1) {
+    return arr[0];
+  } else {
+    return Math.max(arr.shift(), max1(arr));
+  }
+}
+console.log(max1([1, 2, 9, 4, 5, 6, 7]));
+
+//Recursiv - maximul elementelor unui sir 2 - array in array
+
+const max2 = (arr) => {
+  var max = -Infinity;
+
+  for (var i = 0; i < arr.length; i++) {
+    var el = arr[i];
+
+    if (Array.isArray(el)) {
+      el = max2(el);
+    }
+    if (el > max) {
+      max = el;
+    }
+  }
+  return max;
+};
+
+console.log(max2([1, 2, 9, 4, 5, [12, 4, [100, 99], 5], 7]));
+
+//Recursiv - media elementelor din sir
+
+function medie(arr, memo, total) {
+  memo || (memo = 0);
+  total || (total = arr.length);
+  if (arr.length === 0) {
+    return memo / total;
+  }
+  return medie(arr.slice(1, arr.length), memo + arr[0], total);
+}
+console.log(medie([1, 2, 3, 4, 5, 6, 7]));
--- lab8/test.js
+++ lab8/test.js
@@ -38,3 +38,50 @@ console.log(result);
 
 const fact = (x) => (x === 0 ? 1 : x * fact(x - 1));
 console.log(fact(4));
+
+function myLen(arr) {
+  if (_.isEmpty(arr)) {
+    return 0;
+  } else {
+    return 1 + myLen(_.rest(arr));
+  }
+}
+console.log(myLen(_.range(10)));
+console.log(myLen([1, 2, 3]));
+
+//Recursiv - suma elementelor unui sir
+
+const suma = (arr) => (arr.length === 0 ? 0 : arr[0] + suma(arr.slice(1)));
+console.log(suma([1, 2, 3, 4, 5]));
+console.log(suma(_.range(10)));
+
+//Recursiv - maximul elementelor unui sir 1
+
+function max1(arr) {
+  if (arr.length === 1) {
+    return arr[0];
+  } else {
+    return Math.max(arr.shift(), max1(arr));
+  }
+}
+console.log(max1([1, 2, 9, 4, 5, 6, 7]));
+
+//Recursiv - maximul elementelor unui sir 2 - array in array
+
+const max2 = (arr) => {
+  var max = -Infinity;
+
+  for (var i = 0; i < arr.length; i++) {
+    var el = arr[i];
+
+    if (Array.isArray(el)) {
+      el = max2(el);
+    }
+    if (el > max) {
+      max = el;
+    }
+  }
+  return max;
+};
+
+console.log(max2([1, 2, 9, 4, 5, [12, 4, [100, 99], 5], 7]));
